aa3380a3ec9b9eeff3e4b0aaffe5fa91365f2cf7
feibeihaibujiu <feibeihaibujiu@qq.com>
2025-01-10 00:47:05 +0800

    elementPlus自动按需导入

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from'@/components/HelloWorld.vue'
 </script>
 
 <template>
@@ -9,7 +10,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
