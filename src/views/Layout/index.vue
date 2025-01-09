43ea8d64e711d213db2da9934ccf449df950dfff
feibeihaibujiu <feibeihaibujiu@qq.com>
2025-01-10 01:36:49 +0800

    项目整体路由设计

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+
+
+</script>
+  
+<template>
+    <div>
+        <h1>我是首页</h1>
+        <!-- 二级路由接口 -->
+        <RouterView/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Login/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+  
+<template>
+    <h1>我是登录页</h1>
+</template>
+
+<style scoped>
+
+</style>
