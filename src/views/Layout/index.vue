7bf3163a8eb6b166baa59d2c53945e868c0b31bf
feibeihaibujiu <feibeihaibujiu@qq.com>
2025-01-11 02:54:26 +0800

    pinia优化导航数据请求

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -4,6 +4,13 @@ import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 import layoutFixed from './components/layoutFixed.vue';
+import { onMounted } from 'vue';
+import { useCategoryStore } from '@/stores/category'
+//触发pinia动作获取分类导航
+const categoryStore = useCategoryStore()
+onMounted( () => {
+  categoryStore.getCategory()
+})
 </script>
   
 <template>
