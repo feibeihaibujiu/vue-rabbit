521628457e34327b1985a282a4f65aa49d8eb794
feibeihaibujiu <feibeihaibujiu@qq.com>
2025-01-12 01:23:55 +0800

    Home-懒加载指令优化

---
+++ src/directives/index.js
@@ -0,0 +1,26 @@
+//定义懒加载插件
+import { useIntersectionObserver } from "@vueuse/core";
+
+export const lazyPlugin = {
+  install(app) {
+    // 懒加载指令逻辑
+    // 全局指令注册
+    app.directive("img-lazy", {
+      mounted(el, binding) {
+        //el: 指令般的的那个元素 img
+        //binding： binding.value 指令等于号后面绑定的表达式的值  图片url
+        // console.log(el,binding.value);
+        const { stop } = useIntersectionObserver(
+            el, 
+            ([{ isIntersecting }]) => {
+            console.log(isIntersecting);
+            if (isIntersecting) {
+                //进入视图区域
+                el.src = binding.value;
+                stop();
+          }
+        });
+      },
+    });
+  },
+};
