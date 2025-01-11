0f2edc2233727be553fbaf8596f221ca789c693f
feibeihaibujiu <feibeihaibujiu@qq.com>
2025-01-11 23:47:27 +0800

    Home-整体结构搭建和分类实现

---
+++ src/views/Home/components/HomeHot.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+  
+<template>
+    我是产品推荐
+</template>
+
+<style scoped>
+
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
 </script>
-  
+
 <template>
-    <h1>我是home页</h1><div style="height: 500px;"></div>
+    <div class="container">
+        <HomeCategory />
+        <HomeBanner />
+    </div>
+    <HomeNew />
+    <HomeHot />
+    <homeProduct />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
