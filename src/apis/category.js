d18a43380996606270a7f0dc115207a297728aa1
feibeihaibujiu <feibeihaibujiu@qq.com>
2025-01-13 00:34:00 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,10 +1,19 @@
-import httpInstance from '@/utils/http'
+import httpInstance from "@/utils/http";
 
 export const getCategoryAPI = (id) => {
-    return httpInstance({
-      url:'/category',
-      params:{
-        id
-      }
-    })
-  }
+  return httpInstance({
+    url: "/category",
+    params: {
+      id,
+    },
+  });
+};
+
+export const getCategoryFilterAPI = (id) => {
+  return httpInstance({
+    url: "/category/sub/filter",
+    params: {
+      id,
+    },
+  });
+};
