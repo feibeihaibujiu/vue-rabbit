5641824b3e8ecfb3b9de00978d5555cfe8a7651d
feibeihaibujiu <feibeihaibujiu@qq.com>
2025-01-13 17:47:06 +0800

    详情页-整体认识和路由配置

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router';
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
-import layoutFixed from './components/layoutFixed.vue';
+import LayoutFixed from './components/LayoutFixed.vue'
 import { onMounted } from 'vue';
 import { useCategoryStore } from '@/stores/category'
 //触发pinia动作获取分类导航
@@ -14,7 +14,7 @@ onMounted( () => {
 </script>
   
 <template>
-  <layoutFixed />
+  <LayoutFixed />
   <LayoutNav /> 
   <LayoutHeader />
   <RouterView />
