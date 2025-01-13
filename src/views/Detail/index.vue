79b7f3aa2becea613a0cde284adef07f2138e42c
feibeihaibujiu <feibeihaibujiu@qq.com>
2025-01-13 22:50:47 +0800

    详情页-热榜区域实现

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,6 +2,8 @@
 import  { getDetail } from '@/apis/detail'
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import DetailHot from './components/DetailHot.vue';
+
 
 const goods = ref([])
 const route = useRoute()
@@ -118,7 +120,10 @@ onMounted( () => {
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
-
+                <!-- 24小时热榜 -->
+                <DetailHot :type="1"/>
+                <!-- 周热榜 -->
+                <DetailHot :type="2"/>
             </div>
           </div>
         </div>
